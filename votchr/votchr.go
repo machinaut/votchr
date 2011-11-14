ac491aff4a953235bd2bbb8f66bd415809fd8609
Alex Ray <ajray@ncsu.edu>
2011-11-13 22:41:24 -0500

    return 401 unauthorized if not logged in for /votch

--- votchr/votchr.go
+++ votchr/votchr.go
@@ -2,63 +2,72 @@ package votchr
 
 import (
     "appengine"
+    //"appengine/datastore"
     "appengine/user"
     "fmt"
     "http"
-    "template"
+    //"os"
+    //"template"
+    //"time"
 )
 
 func init() {
-    //http.HandleFunc("/", handler)
-    http.HandleFunc("/", root)
-    http.HandleFunc("/sign", sign)
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/votch", votch)
+    http.HandleFunc("/_ah/login_required", openIdHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func votch(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     u := user.Current(c)
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
+    if u != nil {
+        url, err := user.LogoutURL(c, "/")
         if err != nil {
             http.Error(w, err.String(), http.StatusInternalServerError)
             return
         }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
+        fmt.Fprintf(w, "Hello, %s! (<a href='%s'>Sign out</a>)", u, url)
+    } else {
+        unauthorized(w, r)
     }
-    fmt.Fprintf(w, "Hello, %v!", u)
+
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, guestbookForm)
+func hello(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u != nil {
+        url, err := user.LogoutURL(c, "/")
+        if err != nil {
+            http.Error(w, err.String(), http.StatusInternalServerError)
+            return
+        }
+        fmt.Fprintf(w, "Hello, %s! (<a href='%s'>Sign out</a>)", u, url)
+    } else {
+        fmt.Fprintf(w, "Please, <a href='/_ah/login_required'>login</a>.")
+    }
+
 }
 
-const guestbookForm = `
-<html>
-  <body>
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`
+func openIdHandler(w http.ResponseWriter, r *http.Request) {
+    providers := map[string]string {
+        "google"   : "gmail.com",
+    }
 
-func sign(w http.ResponseWriter, r *http.Request) {
-    err := signTemplate.Execute(w, r.FormValue("content"))
-    if err != nil {
-        http.Error(w, err.String(), http.StatusInternalServerError)
+    c := appengine.NewContext(r)
+    fmt.Fprintf(w, "Sign in at: ")
+    for name, url := range providers {
+        login_url, err := user.LoginURLFederated(c, "/", url)
+        if err != nil {
+            http.Error(w, err.String(), http.StatusInternalServerError)
+            return
+        }
+        fmt.Fprintf(w, "[<a href='%s'>%s</a>]", login_url, name)
     }
 }
 
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
-
-const signTemplateHTML = `
-<html>
-  <body>
-    <p>You wrote:</p>
-    <pre>{{html .}}</pre>
-  </body>
-</html>
-`
+// return 401 unauthorized
+func unauthorized(w http.ResponseWriter, req *http.Request){
+    w.Header().Set("Content-Type", "text/plain;" + "charset=utf-8")
+    w.WriteHeader(http.StatusUnauthorized)
+}
